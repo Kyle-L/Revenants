fdcb1052d8354fd8ebc7cfb730d4dc5aa74a4cbd
Kyle Lierer <liererkt@gmail.com>
2020-08-06 19:08:47 +0000

    Increased the length of the ids in the database.

--- app/models.py
+++ app/models.py
@@ -6,13 +6,13 @@ class Rooms(db.Model):
 
 
 class Players(db.Model):
-    id = db.Column(db.String(64), primary_key=True)
+    id = db.Column(db.String(128), primary_key=True)
     username = db.Column(db.String(24), unique=False, nullable=False, default=False)
     code = db.Column(db.String(4), unique=False, nullable=False, default=False)
     ready = db.Column(db.Boolean, unique=False, nullable=False, default=False)
     role = db.Column(db.String(12), unique=False, nullable=False, default='regular')
     is_alive = db.Column(db.Boolean, unique=False, nullable=False, default=True)
     is_marked = db.Column(db.Boolean, unique=False, nullable=False, default=False)
-    chosen_player = db.Column(db.String(24), unique=False, nullable=True)
+    chosen_player = db.Column(db.String(128), unique=False, nullable=True)
     character_name = db.Column(db.String(64), unique=False, nullable=True)
     character_age = db.Column(db.Integer, unique=False, nullable=True)
